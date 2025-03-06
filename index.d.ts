598c58a618724582b7ab27e65ef410ad4f767f34
孑愁 <401828628@qq.com>
2025-03-06 13:36:51 +0800

    移除isSupportAdServices与getAttributionData接口

--- index.d.ts
+++ index.d.ts
@@ -13,38 +13,11 @@ export interface IGetAttributionToken {
     msg: string;
 }
 
-export interface IGetAttributionData {
-    /**
-     * 状态码
-     */
-    code: number;
-    /**
-     * 归因数据
-     */
-    data: any;
-    /**
-     * 错误信息
-     */
-    msg: string;
-}
-
 export default class AdServices {
-    /**
-     * 是否支持 AdServices 调用
-     * iOS only
-     */
-    static isSupportAdServices(): Promise<boolean>;
-
     /**
      * 获取自归因数据 Token(AdServices)
      * 此token需要到 https://api-adservices.apple.com/api/v1/ 换取归因数据
      * iOS only
      */
     static getAttributionToken(): Promise<IGetAttributionToken>
-
-    /**
-     * 获取自归因数据 (iAd)
-     * iOS only
-     */
-    static getAttributionData(): Promise<IGetAttributionData>
 }
--- ios/Madservices.m
+++ ios/Madservices.m
@@ -1,18 +1,9 @@
 #import "Madservices.h"
 #import <AdServices/AdServices.h>
-#import <iAd/iAd.h>
 
 @implementation Madservices
 
 RCT_EXPORT_MODULE()
-
-/**
- * 是否支持 AdServices 调用
- */
-RCT_EXPORT_METHOD(isSupportAdServices: (RCTPromiseResolveBlock)resolve rejecter: (RCTPromiseRejectBlock)reject) {
-    BOOL isSupport = [self isSupportAdServices];
-    resolve(@(isSupport));
-}
 
 /**
  * 获取自归因数据 Token(AdServices)
@@ -34,37 +25,5 @@ RCT_EXPORT_METHOD(getAttributionToken: (RCTPromiseResolveBlock)resolve rejecter:
         resolve(ret);
     }
 }
-
-/**
- * 获取自归因数据 (iAd)
- */
-RCT_EXPORT_METHOD(getAttributionData: (RCTPromiseResolveBlock)resolve rejecter: (RCTPromiseRejectBlock)reject) {
-    if (@available(iOS 14.3, *)) {
-        NSDictionary *ret = @{@"code": @(0), @"data": @{}, @"msg": @"系统版本不支持"};
-        resolve(ret);
-    } else {
-        [[ADClient sharedClient] requestAttributionDetailsWithBlock:^(NSDictionary *attrData, NSError *error) {
-            if (!error) {
-                NSDictionary *ret = @{@"code": @(1), @"data": attrData, @"msg": @""};
-                resolve(ret);
-            } else {
-                NSDictionary *ret = @{@"code": @(-1), @"data": @{}, @"msg": [NSString stringWithFormat: @"%ld", (long)error.code]};
-                resolve(ret);
-            }
-        }];
-    }
-}
-
-
-- (BOOL)isSupportAdServices {
-    BOOL isSupport = true;
-    if (@available(iOS 14.3, *)) {
-        isSupport = true;
-    } else {
-        isSupport = false;
-    }
-    return isSupport;
-}
-
 
 @end
